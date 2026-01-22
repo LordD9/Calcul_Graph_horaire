b9b315c2f51dec81863d0466e59fd786c72055af
LordD9 <balraj.david@outlook.com>
2026-01-22 10:40:42 +0100

    fix typo 2

--- energy_logic.py
+++ energy_logic.py
@@ -32,7 +32,7 @@ def get_default_energy_params():
         "recuperation_pct": 65, # Basé sur l'analyse
 
         # Coeff de Davis (Force en Newtons par tonne)
-        "davis_A_N_t": 16.0  # Résistance mécanique (N/t)
+        "davis_A_N_t": 16.0,  # Résistance mécanique (N/t)
         "davis_B_N_t_kph": 0.115, # Résistance roulements (N/t/kph)
         "davis_C_N_t_kph2": 0.0042, # Résistance aéro (N/t/kph^2)
 
